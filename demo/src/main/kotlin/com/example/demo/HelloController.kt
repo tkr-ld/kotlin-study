446481998e811053f8d225bbd567304c54b0b215
sonobetakuro <sonobetakuro@team-lab.com>
2020-05-04 16:15:42 +0900

    DI追加

--- demo/src/main/kotlin/com/example/demo/HelloController.kt
+++ demo/src/main/kotlin/com/example/demo/HelloController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloController {
+class HelloController(private val greeter: Greeter) {
     @GetMapping("hello")
-    fun hello(@RequestParam ("name") name: String): String = "Hello,$name！"
+    fun hello(@RequestParam ("name") name: String): String = greeter.hello(name)
 }
